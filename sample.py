ade8c1aaae9873e064db48486a301d4f28411b50
usagi-f <usagi.f@gmail.com>
2018-10-16 18:55:32 +0900

    add title screen

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 import pyxel
+import time
 
 class App:
     def __init__(self):
@@ -7,6 +8,9 @@ class App:
 
         # 画像バンクの0版に画像を読み込む
         pyxel.image(0).load(0, 0, "assets/cat_16x16.png")
+
+        # 画面番号
+        self.screen = 0
 
         # catの描画情報
         self.cat_x = 50
@@ -17,7 +21,19 @@ class App:
         pyxel.run(self.update, self.draw)
 
     def update(self): # フレーム更新
-        self.update_cat()
+        # ゲームの終了コマンド
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+        # ルーティング
+        if self.screen == 0:
+            self.update_title()
+        elif self.screen == 1:
+            self.update_cat()
+
+    def update_title(self): # タイトルメニュー
+        if pyxel.btn(pyxel.KEY_ENTER):
+            self.screen = 1
 
     def update_cat(self): # 猫の移動
         if pyxel.btn(pyxel.KEY_LEFT):
@@ -29,6 +45,14 @@ class App:
             self.cat_direction_left = False
 
     def draw(self): # 描画
+        # ルーティング
+        if self.screen == 0:
+            self.draw_title()
+        elif self.screen == 1:
+            self.draw_game()
+
+    def draw_game(self): # メインのゲーム描画
+        self.screen = 1
         pyxel.cls(0)
         pyxel.rect(0, 80, 160, 120, 3)
 
@@ -46,5 +70,12 @@ class App:
             16,
             5,
         )
+
+    def draw_title(self): # タイトル画面描画
+        self.screen = 0
+        pyxel.rect(0, 0, 160, 120, 0)
+        pyxel.text(50, 40, "Sample Cat App", 7)
+        pyxel.text(30, 80, "PRESS ENTER TO GAME START", 10)
+        pyxel.text(5, 110, "PRESS Q TO EXIT", 7)
 
 App()
